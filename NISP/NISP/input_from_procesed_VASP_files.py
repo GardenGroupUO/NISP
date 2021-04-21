1a6e178c02605dae5df0acc3e11b22b9e93a0951
geoffreyweal <geoffrey.weal@gmail.com>
2021-04-22 10:57:01 +1200

    update version of NISP v1.3.4

--- NISP/NISP/input_from_procesed_VASP_files.py
+++ NISP/NISP/input_from_procesed_VASP_files.py
@@ -62,6 +62,7 @@ def input_from_procesed_VASP_files(element,local_optimiser,e_coh,maximum_size):
 def get_VASP_energy(name):
 	path = 'VASP_Clusters/'+name+'/OUTCAR'
 	# do stuff # need to write
+	raise Exception('This method (get_VASP_energy in input_from_procesed_VASP_files) is still to be written, to come back to later on')
 	DFT_energy = 0.0
 	data_obtained_successfully = False
 	return DFT_energy, data_obtained_successfully
